bca14c5496085eeab71b11aa3d7d247755be5e96
Sabinaxon08 <sabdugaffarovasabina@gmaSabinaxon08il.com>
2024-06-16 23:30:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// let son1 = +prompt("son yoz")
+// let son2 = +prompt("son yoz")
+// console.log(son1 * son2)
+
+let ism = prompt("ismizni yozing: ")
+let familiya = prompt("familiya yozing: ")
+let yosh = +prompt(" yoshingizni yozing: ")
+let address = prompt(" address yozing: ")
+
+
+console.log("ism: " + ism + " Type: " + typeof ism)
+console.log("familiya: " + familiya + " Type: " + typeof familiya)
+console.log("yosh: " + yosh + " Type: " + typeof yosh)
+console.log("address: " + address+ " Type: " + typeof address)
